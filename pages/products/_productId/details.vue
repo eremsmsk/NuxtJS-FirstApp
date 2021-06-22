732ae8ae4886bf46ced1ea468f4ec9eaa4004db7
eremsimsek <eremsmsk@gmail.com>
2021-06-22 11:45:17 +0300

    Initial commit

---
+++ pages/products/_productId/details.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>DETAILS PAGE </h1>
+        <h2> {{ $route.params.productId }} </h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/products/_productId/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="container">
+    <h1> products Id page </h1>
+    <h2> Products Id : {{ $route.params.productId }} </h2>
+    </div>    
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
